c98da1338dae32f91574dba03373349cd612730d
Anne-Flower <anneviola8@gmail.com>
2026-02-24 19:29:50 +0100

    functions1

--- function_Employee.c
+++ function_Employee.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 #define MAX_LETTERS 22
@@ -8,10 +9,29 @@ struct Employee {
                 double salary;
         };
 
-//void init_employee(struct Employee *emp);
+void init_employee(struct Employee *emp);
+
+
+        void init_employee(struct Employee *emp) {
+                strcpy( emp->name, "Stevie");
+                emp->salary = 3333.333;
+                printf("employee : %s win %.2f \n", emp->name, emp->salary);
+        }
 
 int main() {
 
-	struct Employee steve = {0};	
-	printf("employee %s is paid %2f \n",steve.name, steve.salary );	
+	struct Employee steve = {
+		.name = "Steve",
+		.salary = 1224356.879,
+	};	
+	printf("employee %s is paid %.2f \n",steve.name, steve.salary );	
+
+	init_employee(&steve);
+
+	printf("address : %p \n", steve.name);
+	printf("address : %p \n", &steve);
+	printf("address : %p \n", &steve.salary);
+
+
+	return 0;
 }
