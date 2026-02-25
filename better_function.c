65ed7af70f0dedb4b5209a2a5dbf108e698a3be1
Anne-Flower <anneviola8@gmail.com>
2026-02-25 18:17:29 +0100

    tests

--- better_function.c
+++ better_function.c
@@ -4,11 +4,11 @@
 
 #define MAX_NAME 255
 
-
-typedef enum {
+typedef enum
+{
 	STATUS_SUCCESS = 0,
-	STATUS_FAILED = -1,
-} status; 
+	STATUS_FAIL = -1,
+} status;
 
 struct Employee
 {
@@ -16,32 +16,48 @@ struct Employee
 	double salary;
 };
 
-status create(struct Employee *emp) {
-	strcpy (emp->name, "Vivi");
+status create(struct Employee *emp)
+{
+	if (emp == NULL)
+	{
+		return STATUS_FAIL;
+	}
+
+	strcpy(emp->name, "Vivi");
 	emp->salary = 333333333.3;
-	printf("here is %s (with init method and pointer), he is paided %.2f and his memory address is : %p \n", emp->name, emp->salary, emp->name );
+
+	printf("here is %s (with init method and pointer), he is paided %.2f and his memory address is : %p \n", emp->name, emp->salary, emp->name);
+
 	return STATUS_SUCCESS;
 };
 
-status destroy(struct Employee *emp) {
-
-	emp->name[0] = 0;
-	emp->salary = 0;
-	return STATUS_SUCCESS;
-};
-
+status destroy(struct Employee *emp);
 
 int main()
 {
-	struct Employee steven = {
-		.name = "Stou",
-		.salary = 23456.78,
+
+	struct Employee toto = {0};
+	struct Employee vava = {
+		.name = "vava",
+		.salary = 4545.78,
 	};
+	printf("name : %s and salary : %.2f and address : %p \n", vava.name, vava.salary, vava.name);
 
-	printf("just initialized employee %s with %.2f of salary, in the simpler way \n %s is located here %p \n", steven.name, steven.salary, steven.name, steven.name);
+	printf("name : %s and salary : %.2f and address : %p \n", toto.name, toto.salary, toto.name);
+	create(&toto);
+	destroy(&vava);
+	printf("name : %s and salary : %.2f and address : %p \n", vava.name, vava.salary, vava.name);
 
-	init(&steven);
-
-	
 	return 0;
+}
+
+status destroy(struct Employee *emp)
+{
+	if (emp == NULL)
+		return STATUS_FAIL;
+
+	emp->name[0] = '\0';
+	emp->salary = 0;
+
+	return STATUS_SUCCESS;
 }
