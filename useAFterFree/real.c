7b15d54183c120e5e9ec8e7b462ce06398503b24
Anne-Flower <anneviola8@gmail.com>
2026-03-23 11:10:29 +0100

    uaf and start pwntools

---
+++ useAFterFree/real.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ANIMAL_NAME_SIZE 64
+
+struct cat
+{
+    char name[ANIMAL_NAME_SIZE];
+    void (*speak)(char *);
+};
+
+struct dog
+{
+    void (*speak)(char *);
+    char name[ANIMAL_NAME_SIZE];
+};
+
+struct cat *bemol = NULL;
+struct dog *mam = NULL;
+
+void cat_speak(char *name)
+{
+    printf("miaou, de %s\n", name);
+}
+
+void dog_speak(char *name)
+{
+    printf("gron, de %s\n", name);
+}
+
+int main()
+{
+    char buff[100];
+
+    while (1)
+    {
+        printf("> ");
+        fgets(buff, sizeof(buff), stdin);
+
+        // create dog
+        if (!strcmp(buff, "newdog\n"))
+        {
+            mam = malloc(sizeof(struct dog));
+            mam->speak = dog_speak;
+            strcpy(mam->name, "maaaam");
+        }
+
+        // create cat
+        else if (!strcmp(buff, "newcat\n"))
+        {
+            bemol = malloc(sizeof(struct cat));
+            bemol->speak = cat_speak;
+            // strcpy(bemol->name, "bemolito");
+            fgets(bemol->name, ANIMAL_NAME_SIZE, stdin);
+        }
+
+        // free dog
+        else if (!strcmp(buff, "deletedog\n"))
+        {
+            free(mam);
+        }
+
+        // free cat
+        else if (!strcmp(buff, "deletecat\n"))
+        {
+            free(bemol);
+        }
+
+        // use dog (=> UAF bang big badaboum)
+        else if (!strcmp(buff, "speakdog\n"))
+        {
+            if (mam)
+                mam->speak(mam->name);
+        }
+
+        // use cat
+        else if (!strcmp(buff, "speakcat\n"))
+        {
+            if (bemol)
+                bemol->speak(bemol->name);
+        }
+    }
+
+    return 0;
+}
