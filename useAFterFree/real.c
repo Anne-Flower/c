0fabd2eb9d0e1d7fa81cf9301a2ed490ee2a0329
Anne-Flower <anneviola8@gmail.com>
2026-03-23 15:36:56 +0100

    test

--- useAFterFree/real.c
+++ useAFterFree/real.c
@@ -38,12 +38,15 @@ int main()
         printf("> ");
         fgets(buff, sizeof(buff), stdin);
 
-        // create dog
+        // create dog and display memory
         if (!strcmp(buff, "newdog\n"))
         {
             mam = malloc(sizeof(struct dog));
             mam->speak = dog_speak;
             strcpy(mam->name, "maaaam");
+            printf("Got dog at %p\n", mam);
+            printf("Name?\n");
+            fgets(mam->name, ANIMAL_NAME_SIZE, stdin);
         }
 
         // create cat
@@ -52,6 +55,8 @@ int main()
             bemol = malloc(sizeof(struct cat));
             bemol->speak = cat_speak;
             // strcpy(bemol->name, "bemolito");
+            printf("Got cat at %p\n", bemol);
+            printf("Name?\n");
             fgets(bemol->name, ANIMAL_NAME_SIZE, stdin);
         }
 
@@ -79,6 +84,17 @@ int main()
         {
             if (bemol)
                 bemol->speak(bemol->name);
+        }
+
+        if (!strcmp(buff, "help\n"))
+        {
+            printf("options:\n");
+            printf("newdog\n");
+            printf("newcat\n");
+            printf("deletedog\n");
+            printf("deletecat\n");
+            printf("printdog\n");
+            printf("printcat\n");
         }
     }
 
